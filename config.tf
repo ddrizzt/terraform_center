e39a0492e9a2b002deaef750694569e90318919d
Eason <ddrizzt@gmail.com>
2019-01-02 16:57:21 +0800

    init

--- config.tf
+++ config.tf
@@ -1,5 +1,5 @@
   provider "aws" {
     region           = "us-west-2"
-    access_key  = "AKIAJ3JC4RUFWIMULVPQ "
-    secret_key   = "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"
+    access_key  = "XXXX "
+    secret_key   = "YYYYY"
 }
